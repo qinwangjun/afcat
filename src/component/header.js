eda28777845f5a6ee52e3adb6d734bd6552b89be
qinwangjun <qinwj001@163.com>
2021-08-19 13:19:09 +0800

    添加退出登录功能

--- src/component/header.js
+++ src/component/header.js
@@ -1,26 +1,29 @@
 import {Layout, Affix, Col, Row} from "antd";
 import { Link } from "react-router-dom";
 import Nav from "./nav";
-// import Drop from "./dropdowns";
 import { navs } from "../routers/router.config";
 import { Popover } from 'antd';
 import { DownOutlined } from '@ant-design/icons';
 import { useSelector } from "react-redux";
 import defaultAvatar from "../static/imgs/peopledefault.png";
+import cookie from 'react-cookies';
 
 const content = (
   <div>
     <div className="upLoadAvatar"><Link to="/head">上传头像</Link></div>
-    {/* <div className="Logout"><Link to="/">退出登录</Link></div> */}
+    <div className="Logout"><Link to="/" onClick={()=>{
+      cookie.remove('token');
+      cookie.remove('avatar');
+      cookie.remove('userName');
+      window.history.go(0);
+    }}>退出登录</Link></div>
   </div>
 );
-// let avatar = '';
-// let userName = '';
-// let isLogin = false;
+
 function Header(){
   const {isLogin} = useSelector(state=>state.guards);
   const {avatar,userName} = useSelector(state=>state.user);
-  
+  // console.log('avatar:',avatar)
   return <Affix offsetTop={0}>
     <Layout.Header>
       <div className="header_flex">
@@ -40,7 +43,7 @@ function Header(){
           <div className="userInfo">
             {isLogin ? <Popover placement="bottom" content={content} title="">
               <Link to="/">
-                <img className="avatar" src={avatar.split('http://39.99.151.246/public/avatar/')[1] == '' ? defaultAvatar : avatar} title=""/>
+                <img className="avatar" src={(avatar.split('http://10.24.24.64:9999/public/avatar/')[1] == '' || avatar.split('http://10.24.24.64:9999')[1] == '') ? defaultAvatar : avatar} title=""/>
                 <span className="userName">
                   {userName} <DownOutlined />
                 </span>
